0193d4c7a1243d60113c1cd303aea8262c9780b6
BL365 <bl365@bk.ru>
2018-02-14 15:44:14 +0300

    сделана табличная верстка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-// import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
-/*import { HttpClientModule } from '@angular/common/http';*/
 
 
 @NgModule({
@@ -16,11 +13,7 @@ import { TasksComponent } from './tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    /*HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )*/
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
